0988096c96a5fdeca5e1ac4ffee1b1741bdb33ff
Musharruf <mdmusharruf13@gmail.com>
2023-04-21 23:36:09 +0530

    structure of single linked list

---
+++ LinkedList/Singly LinkedList/SLLstructure.java
@@ -0,0 +1,13 @@
+public class SinglyLinkedList{
+    Node head;
+
+    static class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
